bdfd6260e185d1ae1132860da1d359d752d2669e
Jeff Frost <jeff.frost@procore.com>
2022-07-26 09:39:48 -0500

    Initial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jfrost/hellogo
+
+go 1.18
+
+require github.com/jfrost/mystrings v0.0.0-20220726143648-55a3bd0f6337 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jfrost/mystrings"
+)
+
+func main() {
+	fmt.Println(mystrings.Reverse("hello world"))
+}
